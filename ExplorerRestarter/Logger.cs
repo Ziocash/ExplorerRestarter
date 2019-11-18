cd334174223fd0498df9374e13777b06d4a178cc
Ziocash <Ziocash@users.noreply.github.com>
2019-11-18 11:27:07 +0100

    Project implementation
    [+] General implementation

---
+++ ExplorerRestarter/Logger.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Build.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ExplorerRestarter
+{
+    public enum Severity
+    {
+        Critical = 9,
+        Exception = 3,
+        Log = 0
+    }
+    public class Logger 
+    {
+        public readonly string Filename = "log.log";
+
+        public void Log(Severity severity, string message)
+        {
+            if (severity == Severity.Critical)
+            {
+                Write(message);
+                MessageBox.Show(message, "Critical error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+                Write(message);
+        }
+
+        private void Write(string message)
+        {
+            using (FileStream fs = new FileStream(Filename, FileMode.Append))
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                sw.WriteLine($"[{DateTime.Now.ToLongTimeString()}]: {message}");
+                sw.Flush();
+            }
+        }
+
+    }
+}
---
+++ ExplorerRestarter/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ExplorerRestarter
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer = new DispatcherTimer();
+        private List<Process> processes;
+        private Logger logger;
+        public MainWindow()
+        {
+            InitializeComponent();
+            logger = new Logger();
+            timer.Interval = TimeSpan.FromMilliseconds(200);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+            logger.Log(Severity.Log, "Initialization completed");
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            UpdateInfo();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateInfo();
+        }
+
+        private void RestartButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                processes[0].Kill();
+                logger.Log(Severity.Log, "Restart completed");
+            }
+            catch(Exception ex)
+            {
+                logger.Log(Severity.Exception, ex.Message);
+            }
+        }
+
+        private void UpdateInfo()
+        {
+            try
+            {
+                processes = new List<Process>(Process.GetProcessesByName("explorer").AsEnumerable());
+                RunExplorerPID.Text = processes[0].Id.ToString();
+                if (processes[0].Responding)
+                {
+                    RunExplorerResponse.Text = "Responding";
+                    RunExplorerResponse.Foreground = Brushes.Green;
+                }
+                else
+                {
+                    RunExplorerResponse.Text = "Not responding";
+                    RunExplorerResponse.Foreground = Brushes.Red;
+                }
+            }
+            catch(Exception ex)
+            {                   
+                logger.Log(Severity.Exception, ex.Message);
+            }
+        }
+    }
+}
